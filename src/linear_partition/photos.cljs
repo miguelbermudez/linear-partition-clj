74e3ce3ea86035ab7ac4342c9c752c30cd73a866
Miguel Bermudez <miguel@miguelbermudez.com>
2014-04-13 11:37:49 -0400

    update photos ns

--- src/linear_partition/photos.cljs
+++ src/linear_partition/photos.cljs
@@ -1,13 +1,36 @@
-(ns linear-partition.photos)
+(ns linear-partition.photos
+  (:require [goog.events :as events]))
+
+(defn log [& args]
+  (js/console.log (apply pr-str args)))
 
 (defrecord Photo [^:mutable img url]
   Object
   (init [this] 
+    "Preload Photo with url"
+    ;; set img src
     (set! (.-src img) url)
+    ;; attach onload event to img
+    (events/listen
+      img events/EventType.LOAD
+      (fn [evt]
+        ;(.dir js/console evt)
+        (log (str "[LOAD] " (.-src (.-target evt))))
+        ;(log (.aspect this))
+        
+        ))
+    ;; return img
     img)
-  (aspect [this] (/ (.-width img)(.-height img))))
+
+  (aspect [this]
+    "Get the apect ratio of the photo"
+    (/ (.-width img)(.-height img)))
+  
+  )
 
 (defn make-photo [url]
+  "Factory for creating Photos.
+   Returns false if the url is invalid"
   (let [img (js/Image.)]
     (if-not (= url nil)
       (Photo. img url) 
