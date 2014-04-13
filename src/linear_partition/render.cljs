2b1cb4c59590b4cca27028bc1a6dda9a6c6ff2ff
Miguel Bermudez <miguel@miguelbermudez.com>
2014-04-13 11:36:35 -0400

    preload images

--- src/linear_partition/render.cljs
+++ src/linear_partition/render.cljs
@@ -3,8 +3,8 @@
 (def viewport-width (.-clientWidth (.-body js/document)))
 (def ideal-height (/ (.-clientHeight (.-body js/document)) 2))
 
+
 (defn preload-imgs [coll]
-  (doseq [x (take 5 coll)]
-    (.log js/console "preload: " x)
-    (.init x)))
+  (dorun (map #(.init %) (take 5 coll))))
+
 
